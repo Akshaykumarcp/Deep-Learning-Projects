feb08097ab54cb16e2d7bf1d4028a360698c689e
Akshaykumarcp <akshai.148@gmail.com>
2020-03-03 11:10:10 +0530

    Updated with hyperparameter optimization

--- churn-modelling/churn_modelling.py
+++ churn-modelling/churn_modelling.py
@@ -104,9 +104,124 @@ CM = confusion_matrix(y_test,y_pred)
 
 # calculate the accuracy
 
+print(CM)
+
 from sklearn.metrics import accuracy_score
 
 acc_score = accuracy_score(y_pred,y_test)
 
+print(acc_score)
+
 # training , test and validation accuracy are similar i,e perfect model
+
+# next video
+# hpyerparamter tuning, how many hiden neurons and layers to be used in ANN. General way we'll c while creating ANN
+
+# perform hyperparameter optimization
+
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Embedding, Flatten, LeakyReLU, BatchNormalization
+from keras.activations import relu, sigmoid
+
+'''
+def create_model(layers, activation): # layrs and type of activation
+    model = Sequential() # initiaze sequential model
+    for i, nodes in enumerate(layers):
+        # first layer
+        if i==0: 
+            model.add(Dense(nodes,input_dim=X_train.shape[1])) # first layer providing input dimention 
+            model.add(Activation(activation)) # specifies which type of activation needed
+            model.add(Dropout(0.3)) # applying dropout rate
+        else:
+            # creating all the hidden layers
+            model.add(Dense(nodes))
+            model.add(Activation(activation)) # apply activ
+            model.add(Dropout(0.3))
+            
+            #last layer. units = output = 1. based on research uniform works well
+    model.add(Dense(units=1,kernel_initializer='glorot_uniform',activation='sigmoid'))
+    
+    model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+    return model
+
+# build_fn = created function
+model = KerasClassifier(build_fn=create_model,verbose=0)
+
+# in NN 20, 1 HL 20 Neuron.
+layers =[[20],[40,20],[45,30,15]]
+activations = ['sigmoid','relu'] # example : first iteration sigmoid, second itration rely for 20
+param_grid = dict(layers=layers,activation=activations,batch_size = [128,256],epochs=[30])
+grid = GridSearchCV(estimator=model,param_grid=param_grid,cv=5) # cv of 5 experiments
+
+grid_result = grid.fit(X_train,y_train)
+'''
+
+def create_model(layers, activation):
+    model = Sequential()
+    for i, nodes in enumerate(layers):
+        if i==0:
+            model.add(Dense(nodes,input_dim=X_train.shape[1]))
+            model.add(Activation(activation))
+            model.add(Dropout(0.3))
+        else:
+            model.add(Dense(nodes))
+            model.add(Activation(activation))
+            model.add(Dropout(0.3))
+            
+    model.add(Dense(units = 1, kernel_initializer= 'glorot_uniform', activation = 'sigmoid')) # Note: no activation beyond this point
+    
+    model.compile(optimizer='adam', loss='binary_crossentropy',metrics=['accuracy'])
+    return model
+    
+model = KerasClassifier(build_fn=create_model, verbose=0)
+
+
+layers = [[20], [40, 20], [45, 30, 15]]
+activations = ['sigmoid', 'relu']
+param_grid = dict(layers=layers, activation=activations, batch_size = [128, 256], epochs=[30])
+grid = GridSearchCV(estimator=model, param_grid=param_grid,cv=5)
+
+grid_result = grid.fit(X_train, y_train)
+
+'''
+[0.8387499995157123,
+ {'activation': 'relu',
+  'batch_size': 256,
+  'epochs': 30,
+  'layers': [45, 30, 15]}]
+'''
+
+print(grid_result.best_score_,grid_result.best_params_)
+
+
+pred_y = grid.predict(X_test)
+
+y_pred = (pred_y > 0.5)
+
+y_pred
+
+'''
+
+array([[False],
+       [False],
+       [False],
+       ...,
+       [False],
+       [False],
+       [False]])
+'''
+
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+cm
+
+from sklearn.metrics import accuracy_score
+score=accuracy_score(y_test,y_pred)
+
+score
 
